fdb98d544776f97dc592647dd6097c00e1e4024e
Robbie Hickey <robbie.hickey@msionline.com>
2016-10-18 15:05:59 -0500

    Extract params for easier experimentation

--- itsgonnarain/itsgonnarain.js
+++ itsgonnarain/itsgonnarain.js
@@ -1,19 +1,22 @@
 let audioContext = new AudioContext();
+let loopStart = 16.5;
+let loopDuration = .8;
+let phaseShiftRate = 1.002;
 
 let startLoop = function(audioBuffer, pan = 0, rate = 1) {
     let sourceNode = audioContext.createBufferSource();
     let pannerNode = audioContext.createStereoPanner();
     sourceNode.buffer = audioBuffer;
     sourceNode.loop = true;
-    sourceNode.loopStart = 16.5;
-    sourceNode.loopEnd = 17.3;
+    sourceNode.loopStart = loopStart;
+    sourceNode.loopEnd = loopStart + loopDuration;
     sourceNode.playbackRate.value = rate;
     // StereoPannerNode has a pan attribute that can be set to a number between -1 (all the way to the left) and 1 (all the way to the right)
     pannerNode.pan.value = pan;
 
     sourceNode.connect(pannerNode);
     pannerNode.connect(audioContext.destination);
-    sourceNode.start(0, 16.5);
+    sourceNode.start(0, loopStart);
 };
 
 fetch('itsgonnarain.mp3')
@@ -21,6 +24,6 @@ fetch('itsgonnarain.mp3')
   .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer))
   .then(audioBuffer => {
     startLoop(audioBuffer, -1);
-    startLoop(audioBuffer, 1, 1.002);
+    startLoop(audioBuffer, 1, phaseShiftRate);
   })
   .catch(e => console.error(e));
