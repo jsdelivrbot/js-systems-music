91b7f575fe24baee31d3e19b7b6b96787dd4fa23
Robbie Hickey <robbie.hickey@msionline.com>
2016-10-18 14:53:07 -0500

    Add stereo panning

--- itsgonnarain/itsgonnarain.js
+++ itsgonnarain/itsgonnarain.js
@@ -1,15 +1,25 @@
 let audioContext = new AudioContext();
 
-fetch('itsgonnarain.mp3')
-  .then(response => response.arrayBuffer())
-  .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer))
-  .then(audioBuffer => {
+let startLoop = function(audioBuffer, pan = 0) {
     let sourceNode = audioContext.createBufferSource();
+    let pannerNode = audioContext.createStereoPanner();
     sourceNode.buffer = audioBuffer;
     sourceNode.loop = true;
     sourceNode.loopStart = 16.5;
     sourceNode.loopEnd = 17.3;
-    sourceNode.connect(audioContext.destination);
+    // StereoPannerNode has a pan attribute that can be set to a number between -1 (all the way to the left) and 1 (all the way to the right)
+    pannerNode.pan.value = pan;
+
+    sourceNode.connect(pannerNode);
+    pannerNode.connect(audioContext.destination);
     sourceNode.start(0, 16.5);
+};
+
+fetch('itsgonnarain.mp3')
+  .then(response => response.arrayBuffer())
+  .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer))
+  .then(audioBuffer => {
+    startLoop(audioBuffer, -1);
+    startLoop(audioBuffer, 1);
   })
   .catch(e => console.error(e));
